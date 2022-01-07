7883c839ab1ba3b8e16c6e42fd7ddc4a70e725ec
SabinaMstan <93779021+SabinaMstan@users.noreply.github.com>
2022-01-07 21:20:15 +0200

    Pentru n butoane
    Ghiceste butonul din n butoane

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
-document.addEventListener('DOMContentLoaded', function() {
-  let container = document.getElementById('container');
-  let winButton = getRandomNumber();
-  for (let i = 0; i < 3; ++i) {
-    let button = document.createElement('button');
-    button.type = 'button';
-    button.innerHTML = 'Pick me';
-    button.className = 'btn-styled';
-    button.onclick = function() {
-        if (winButton === i) {
-          alert('Excelent!');
-          location.reload();
-        } else {
-          alert('Duh');
-        }
-    };
-
-    container.appendChild(button);
-  }
-}, false);
-
-
 function getRandomNumber() {
   return Math.floor(Math.random() * 3);
+}
+
+function restartGame() {
+  location.reload();
+}
+
+function getInputFromTextBox() {
+    let input = document.getElementById("userInput").value;
+    if (input >= 3 && input <= 25) {
+    let container = document.getElementById('container');
+    let winButton = getRandomNumber();
+    for (let i = 0; i < input; ++i) {
+      let button = document.createElement('button');
+      button.type = 'button';
+      button.innerHTML = 'Pick me';
+      button.className = 'pickButton';
+      button.onclick = function() {
+          if (winButton === i) {
+            alert('Excelent!');
+            restartGame();
+          } else {
+            alert('Duh');
+          }
+      };
+      container.appendChild(button);
+    }
+  } else {
+    alert('Choose a number between 3 and 25.');
+    restartGame();
+  }
 }
